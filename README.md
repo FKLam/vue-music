d461442de7abef9426c1187fa2c40047394ddc58
阿房宫赋 <1783956729@qq.com>
2020-08-26 15:46:04 +0800

    4）添加歌手列表固定标题的组件；

--- README.md
+++ README.md
@@ -1,21 +1,6 @@
 # vue-music
 
-> 音乐播放器
-
-## Build Setup
-
-``` bash
-# install dependencies
-npm install
-
-# serve with hot reload at localhost:8080
-npm run dev
-
-# build for production with minification
-npm run build
-
-# build for production and view the bundle analyzer report
-npm run build --report
+> Vue 仿网易云Web App音乐播放器
 ```
 
 For a detailed explanation on how things work, check out the [guide](http://vuejs-templates.github.io/webpack/) and [docs for vue-loader](http://vuejs.github.io/vue-loader).
--- src/base/listview/listview.vue
+++ src/base/listview/listview.vue
@@ -31,6 +31,9 @@
         </li>
       </ul>
     </div>
+    <div class="list-fixed" v-show="fixedTitle" ref="fixed">
+      <h1 class="fixed-title">{{fixedTitle}}</h1>
+    </div>
 	</Scroll>
 </template>
 
@@ -38,6 +41,7 @@
 import Scroll from 'base/scroll/scroll'
 import {getData} from 'common/js/dom'
 const ANCHOR_HEIGHT = 18
+const TITEL_HEIGHT = 30
 export default {
   created () {
     this.touch = {}
@@ -48,7 +52,8 @@ export default {
   data () {
     return {
       scrollY: -1,
-      currentIndex: 0
+      currentIndex: 0,
+      diff: -1
     }
   },
   props: {
@@ -62,11 +67,20 @@ export default {
       return this.data.map((group) => {
         return group.title.substr(0, 1)
       })
+    },
+    fixedTitle () {
+      if (this.scrollY > 0) {
+        return ''
+      }
+      return this.data[this.currentIndex] ? this.data[this.currentIndex].title : ''
     }
   },
   methods: {
     onShortcutTouchStart (e) {
       let anchorIndex = getData(e.target, 'index')
+      if (!anchorIndex) {
+        return
+      }
       let firstTouch = e.touches[0]
       this.touch.y1 = firstTouch.pageY
       this.touch.anchorIndex = anchorIndex
@@ -83,6 +97,11 @@ export default {
       this.scrollY = pos.y
     },
     _scrollTo (index) {
+      if (index < 0) {
+        index = 0
+      } else if (index > this.listHeight.length - 2) {
+        index = this.listHeight.length - 2
+      }
       this.$refs.listview.scrollToElement(this.$refs.listGroup[index], 0)
     },
     _calculateHeight () {
@@ -116,11 +135,20 @@ export default {
         let height2 = listHeight[i + 1]
         if (-newY >= height && -newY < height2) {
           this.currentIndex = i
+          this.diff = height2 + newY
           return
         }
       }
       // 滚动到底部，-newY大于最后一个元素的上限
       this.currentIndex = listHeight.length - 2
+    },
+    diff (newVal) {
+      let fixedTop = (newVal > 0 && newVal < TITEL_HEIGHT) ? newVal - TITEL_HEIGHT : 0
+      if (this.fixedTop === fixedTop) {
+        return
+      }
+      this.fixedTop = fixedTop
+      this.$refs.fixed.style.transform = `translate3d(0,${fixedTop}px,0)`
     }
   },
   components: {
@@ -176,21 +204,21 @@ export default {
         font-size: $font-size-small
         &.current
           color: $color-theme
-      .list-fixed
-        position: absolute
-        top: 0
-        left: 0
-        width: 100%
-        .fixed-title
-          height: 30px
-          line-height: 30px
-          padding-left: 20px
-          font-size: $font-size-small
-          color: $color-text-l
-          background: $color-highlight-background
-      .loading-container
-        position: absolute
-        width: 100%
-        top: 50%
-        transform: translateY(-50%)
+    .list-fixed
+      position: absolute
+      top: 0
+      left: 0
+      width: 100%
+      .fixed-title
+        height: 30px
+        line-height: 30px
+        padding-left: 20px
+        font-size: $font-size-small
+        color: $color-text-l
+        background: $color-highlight-background
+    .loading-container
+      position: absolute
+      width: 100%
+      top: 50%
+      transform: translateY(-50%)
 </style>
