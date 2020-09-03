bb883370dd88d90a5cf22aabfbc4944b68e6e67c
阿房宫赋 <1783956729@qq.com>
2020-09-03 15:28:26 +0800

    1）完成歌曲列表组件；

--- src/common/js/mixin.js
+++ src/common/js/mixin.js
@@ -1,4 +1,6 @@
-import {mapGetters} from 'vuex'
+import {playMode} from 'common/js/config'
+import {mapGetters, mapMutations, mapActions} from 'vuex'
+import {shuffle} from 'common/js/util'
 export const playlistMixin = {
   computed: {
     ...mapGetters([
@@ -20,5 +22,83 @@ export const playlistMixin = {
     handlePlayList () {
       throw new Error('组件必须实现 handlePlayList 方法')
     }
+  }
+}
+
+export const playerMixin = {
+  computed: {
+    iconMode () {
+      if (this.mode === playMode.sequence) {
+        return 'icon-next'
+      } else if (this.mode === playMode.loop) {
+        return 'icon-loop'
+      } else {
+        return 'icon-random'
+      }
+    },
+    ...mapGetters([
+      'playList',
+      'currentSong',
+      'mode',
+      'sequenceList'
+    ])
+  },
+  methods: {
+    changeMode () {
+      const mode = (this.mode + 1) % 3
+      this.setPlayMode(mode)
+      let list = null
+      if (mode === playMode.random) {
+        list = shuffle(this.sequenceList)
+      } else {
+        list = this.sequenceList
+      }
+      this.retsetCurrentIndex(list)
+      this.setPlayList(list)
+    },
+    retsetCurrentIndex (list) {
+      let index = list.findIndex((item) => {
+        return item.id === this.currentSong.id
+      })
+      this.setCurrentIndex(index)
+    },
+    ...mapMutations({
+      setCurrentIndex: 'SET_CURRENT_INDEX',
+      setPlayMode: 'SET_PLAY_MODE',
+      setPlayList: 'SET_PLAYLIST',
+      setPlayingState: 'SET_PLAYING_STATE'
+    })
+  }
+}
+
+export const searchMixin = {
+  data () {
+    return {
+      query: '',
+      refreshDelay: 120
+    }
+  },
+  computed: {
+    ...mapGetters([
+      'searchHistory'
+    ])
+  },
+  methods: {
+    onQueryChange (newQuery) {
+      this.query = newQuery
+    },
+    blurInput () {
+      this.$refs.searchBox.blur()
+    },
+    saveSearch () {
+      this.saveSearchHistory(this.query)
+    },
+    addQuery (query) {
+      this.$refs.searchBox.setQuery(query)
+    },
+    ...mapActions([
+      'saveSearchHistory',
+      'deleteSearchHistory'
+    ])
   }
 }
--- src/components/search/search.vue
+++ src/components/search/search.vue
@@ -4,7 +4,7 @@
 			<search-box @query="onQueryChange" ref="searchBox"></search-box>
 		</div>
 		<div class="shortcut-wrapper" v-show="!query" ref="shortcutWrapper">
-			<scroll class="shortcut" ref="shortcut" :data="shortcut">
+			<scroll class="shortcut" ref="shortcut" :data="shortcut" :refreshDelay="refreshDelay">
 				<div>
 					<div class="hot-key">
 						<h1 class="title">热门搜索</h1>
@@ -40,11 +40,11 @@ import Suggest from 'components/suggest/suggest'
 import Scroll from 'base/scroll/scroll'
 import SearchList from 'base/search-list/search-list'
 import Confirm from 'base/confirm/confirm'
-import {mapActions, mapGetters} from 'vuex'
-import {playlistMixin} from 'common/js/mixin'
+import {mapActions} from 'vuex'
+import {playlistMixin, searchMixin} from 'common/js/mixin'
 
 export default {
-  mixins: [playlistMixin],
+  mixins: [playlistMixin, searchMixin],
   components: {
     SearchBox,
     Suggest,
@@ -54,41 +54,24 @@ export default {
   },
   data () {
     return {
-      hotKey: [],
-      query: ''
+      hotKey: []
     }
   },
   computed: {
     shortcut () {
       return this.hotKey.concat(this.searchHistory)
-    },
-    ...mapGetters([
-      'searchHistory'
-    ])
+    }
   },
   created () {
     this._getHotKey()
   },
   methods: {
-    onQueryChange (newQuery) {
-      this.query = newQuery
-    },
     async _getHotKey () {
       let res = await this.$mutils.fetchData(`/search/hot`)
       console.log(res)
       if (res.data && res.data.result) {
         this.hotKey = res.data.result.hots
       }
-    },
-    addQuery (query) {
-      this.$refs.searchBox.setQuery(query)
-    },
-    blurInput () {
-      this.$refs.searchBox.blur()
-    },
-    saveSearch () {
-      console.log('saveSearch')
-      this.saveSearchHistory(this.query)
     },
     deleteOne (query) {
       this.deleteSearchHistory(query)
@@ -107,8 +90,6 @@ export default {
       this.$refs.suggest.refresh()
     },
     ...mapActions([
-      'saveSearchHistory',
-      'deleteSearchHistory',
       'clearSearchHistory'
     ])
   },
